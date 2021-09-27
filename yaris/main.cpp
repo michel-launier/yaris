17e4ecd795c2b5eb16bf57153afd756b819ebef7
Michel Launier <michel.launier@icloud.com>
2021-09-26 20:41:58 -0400

    Initial Commit

---
+++ yaris/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  yaris
+//
+//  Created by Michel Launier on 2021-09-26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
