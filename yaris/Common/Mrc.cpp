a269175f06da9fe6a0a306ada8a05e05f9a3b300
Michel Launier <michel.launier@icloud.com>
2021-09-28 14:39:34 -0400

    Added comment in the MRC files.

--- yaris/Common/Mrc.cpp
+++ yaris/Common/Mrc.cpp
@@ -7,9 +7,15 @@
 
 #include "Mrc.hpp"
 
+// ---------------------------------------------------------------------------
+/// Creation & destruction of the MRC pointer
+///
 _MrcPtr::_MrcPtr(Mrc* p) { if (p != 0)   p->retain(); ptr = p; }
 _MrcPtr::~_MrcPtr()      { if (ptr != 0) ptr->release(); }
 
+// ---------------------------------------------------------------------------
+/// Reassignment of the MRC pointer.
+///
 Mrc* _MrcPtr::assign(Mrc* p) {
     if (p != ptr) {
         if (p != 0)     p->retain();
